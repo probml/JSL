c33b595bcabe39666d4a1c1352923172dce9ed5a
Gerardo Durán-Martín <g.duran@me.com>
2021-12-22 18:27:30 +0000

    feat: ekf_continuous_demo.py
    Add main() function

--- scripts/ekf_continuous_demo.py
+++ scripts/ekf_continuous_demo.py
@@ -14,9 +14,6 @@ from math import ceil
 from jax import random
 from jsl.nlds.continuous_extended_kalman_filter import ContinuousExtendedKalmanFilter
 
-plt.rcParams["axes.spines.right"] = False
-plt.rcParams["axes.spines.top"] = False
-
 def fz(x):
     x, y = x
     return  jnp.asarray([y, x - x ** 3])
@@ -25,45 +22,54 @@ def fx(x):
     x, y = x
     return jnp.asarray([x, y])
 
-dt = 0.01
-T = 7.5
-nsamples = 70
-x0 = jnp.array([0.5, -0.75])
+def main():
+    dt = 0.01
+    T = 7.5
+    nsamples = 70
+    x0 = jnp.array([0.5, -0.75])
 
-# State noise
-Qt = jnp.eye(2) * 0.001
-# Observed noise
-Rt = jnp.eye(2) * 0.01
+    # State noise
+    Qt = jnp.eye(2) * 0.001
+    # Observed noise
+    Rt = jnp.eye(2) * 0.01
 
-key = random.PRNGKey(314)
-ekf = ContinuousExtendedKalmanFilter(fz, fx, Qt, Rt)
-sample_state, sample_obs, jump = ekf.sample(key, x0, T, nsamples)
-mu_hist, V_hist = ekf.estimate(sample_state, sample_obs, jump, dt)
+    key = random.PRNGKey(314)
+    ekf = ContinuousExtendedKalmanFilter(fz, fx, Qt, Rt)
+    sample_state, sample_obs, jump = ekf.sample(key, x0, T, nsamples)
+    mu_hist, V_hist = ekf.estimate(sample_state, sample_obs, jump, dt)
 
-vmin, vmax, step = -1.5, 1.5 + 0.5, 0.5
-X = np.mgrid[-1:1.5:step, vmin:vmax:step][::-1]
-X_dot = jnp.apply_along_axis(fz, 0, X)
+    vmin, vmax, step = -1.5, 1.5 + 0.5, 0.5
+    X = np.mgrid[-1:1.5:step, vmin:vmax:step][::-1]
+    X_dot = jnp.apply_along_axis(fz, 0, X)
 
-fig, ax = plt.subplots()
-ax.plot(*sample_state.T, label="state space")
-ax.scatter(*sample_obs.T, marker="+", c="tab:green", s=60, label="observations")
-field = ax.streamplot(*X, *X_dot, density=1.1, color="#ccccccaa")
-ax.legend()
-plt.axis("equal")
-ax.set_title("State Space")
-# pml.savefig("ekf-state-space.pdf")
+    dict_figures = {}
 
-fig, ax = plt.subplots()
-ax.plot(*sample_state.T, c="tab:orange", label="EKF estimation")
-ax.scatter(*sample_obs.T, marker="+", s=60, c="tab:green", label="observations")
-ax.scatter(*mu_hist[0], c="black", zorder=3)
-for mut, Vt in zip(mu_hist[::4], V_hist[::4]):
-    plot_utils.plot_ellipse(Vt, mut, ax, plot_center=False, alpha=0.9, zorder=3)
-plt.legend()
-field = ax.streamplot(*X, *X_dot, density=1.1, color="#ccccccaa")
-ax.legend()
-plt.axis("equal")
-ax.set_title("Approximate Space")
-plt.savefig("ekf-estimated-space.pdf")
+    fig, ax = plt.subplots()
+    ax.plot(*sample_state.T, label="state space")
+    ax.scatter(*sample_obs.T, marker="+", c="tab:green", s=60, label="observations")
+    field = ax.streamplot(*X, *X_dot, density=1.1, color="#ccccccaa")
+    ax.legend()
+    plt.axis("equal")
+    ax.set_title("State Space")
+    dict_figures["ekf-state-space"] = fig
 
-plt.show()
+    fig, ax = plt.subplots()
+    ax.plot(*sample_state.T, c="tab:orange", label="EKF estimation")
+    ax.scatter(*sample_obs.T, marker="+", s=60, c="tab:green", label="observations")
+    ax.scatter(*mu_hist[0], c="black", zorder=3)
+    for mut, Vt in zip(mu_hist[::4], V_hist[::4]):
+        plot_utils.plot_ellipse(Vt, mut, ax, plot_center=False, alpha=0.9, zorder=3)
+    plt.legend()
+    field = ax.streamplot(*X, *X_dot, density=1.1, color="#ccccccaa")
+    ax.legend()
+    plt.axis("equal")
+    ax.set_title("Approximate Space")
+    dict_figures["ekf-estimated-space"] = fig
+
+    return dict_figures
+
+if __name__ == "__main__":
+    plt.rcParams["axes.spines.right"] = False
+    plt.rcParams["axes.spines.top"] = False
+    dict_figures = main()
+    plt.show()
