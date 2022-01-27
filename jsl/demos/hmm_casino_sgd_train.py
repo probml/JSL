847f12efc233ff0ab3912cc62d29a08001708280
Gerardo Durán Martín <g.duran@me.com>
2022-01-27 14:14:01 -0600

    refactor: hmm_casino_sgd_train.py

--- jsl/demos/hmm_casino_sgd_train.py
+++ jsl/demos/hmm_casino_sgd_train.py
@@ -1,4 +1,4 @@
-'''
+"""
 This demo does MAP estimation of an HMM using gradient-descent algorithm applied to the log marginal likelihood.
 It includes
 
@@ -7,88 +7,93 @@ It includes
 3. Stochastic Gradient Descent
 
 Author: Aleyna Kara(@karalleyna)
-'''
-import superimport
-
-from jax.random import split, randint, PRNGKey
-import jax.numpy as jnp
+"""
+# import superimport
 
 import matplotlib.pyplot as plt
+import jax.numpy as jnp
+from jax.example_libraries import optimizers
+from jax.random import split, randint, PRNGKey
+from jsl.hmm.hmm_sgd_lib import fit
+from jsl.hmm.hmm_utils import pad_sequences, hmm_sample_n
+from jsl.hmm.hmm_discrete_lib import HMMJax, hmm_sample_jax, hmm_plot_graphviz
 
-from hmm_discrete_lib import HMMJax
-from hmm_discrete_lib import hmm_sample_jax, hmm_plot_graphviz
+def main():
+    # state transition matrix
+    A = jnp.array([
+        [0.95, 0.05],
+        [0.10, 0.90]])
 
-from hmm_sgd_lib import fit
-from hmm_utils import pad_sequences, hmm_sample_n
-from jax.experimental import optimizers
+    # observation matrix
+    B = jnp.array([
+        [1/6, 1/6, 1/6, 1/6, 1/6, 1/6], # fair die
+        [1/10, 1/10, 1/10, 1/10, 1/10, 5/10] # loaded die
+    ])
 
-# state transition matrix
-A = jnp.array([
-    [0.95, 0.05],
-    [0.10, 0.90]])
+    pi = jnp.array([1, 1]) / 2
 
-# observation matrix
-B = jnp.array([
-    [1/6, 1/6, 1/6, 1/6, 1/6, 1/6], # fair die
-    [1/10, 1/10, 1/10, 1/10, 1/10, 5/10] # loaded die
-])
+    casino = HMMJax(A, B, pi)
+    num_hidden, num_obs = 2, 6
 
-pi = jnp.array([1, 1]) / 2
+    seed = 0
+    rng_key = PRNGKey(seed)
+    rng_key, rng_sample = split(rng_key)
 
-casino = HMMJax(A, B, pi)
-num_hidden, num_obs = 2, 6
+    n_obs_seq, max_len = 4, 5000
+    num_epochs = 400
 
-seed = 0
-rng_key = PRNGKey(seed)
-rng_key, rng_sample = split(rng_key)
+    observations, lens = pad_sequences(*hmm_sample_n(casino, hmm_sample_jax, n_obs_seq, max_len, rng_sample))
+    optimizer = optimizers.momentum(step_size=1e-3, mass=0.95)
 
-n_obs_seq, max_len = 4, 5000
-num_epochs = 400
+    # Mini Batch Gradient Descent
+    batch_size = 2
+    params_mbgd,losses_mbgd =  fit(observations,
+                                lens,
+                                num_hidden,
+                                num_obs,
+                                batch_size,
+                                optimizer,
+                                rng_key=None,
+                                num_epochs=num_epochs)
 
-observations, lens = pad_sequences(*hmm_sample_n(casino, hmm_sample_jax, n_obs_seq, max_len, rng_sample))
-optimizer = optimizers.momentum(step_size=1e-3, mass=0.95)
+    # Full Batch Gradient Descent
+    batch_size = n_obs_seq
+    params_fbgd,losses_fbgd =  fit(observations,
+                                lens,
+                                num_hidden,
+                                num_obs,
+                                batch_size,
+                                optimizer,
+                                rng_key=None,
+                                num_epochs=num_epochs)
 
-# Mini Batch Gradient Descent
-batch_size = 2
-params_mbgd,losses_mbgd =  fit(observations,
-                               lens,
-                               num_hidden,
-                               num_obs,
-                               batch_size,
-                               optimizer,
-                               rng_key=None,
-                               num_epochs=num_epochs)
+    # Stochastic Gradient Descent
+    batch_size = 1
+    params_sgd,losses_sgd =  fit(observations,
+                                lens,
+                                num_hidden,
+                                num_obs,
+                                batch_size,
+                                optimizer,
+                                rng_key=None,
+                                num_epochs=num_epochs)
 
-# Full Batch Gradient Descent
-batch_size = n_obs_seq
-params_fbgd,losses_fbgd =  fit(observations,
-                               lens,
-                               num_hidden,
-                               num_obs,
-                               batch_size,
-                               optimizer,
-                               rng_key=None,
-                               num_epochs=num_epochs)
+    losses = [ losses_sgd, losses_mbgd, losses_fbgd]
+    titles = [ "Stochastic Gradient Descent", "Mini Batch Gradient Descent", "Full Batch Gradient Descent"]
 
-# Stochastic Gradient Descent
-batch_size = 1
-params_sgd,losses_sgd =  fit(observations,
-                             lens,
-                             num_hidden,
-                             num_obs,
-                             batch_size,
-                             optimizer,
-                             rng_key=None,
-                             num_epochs=num_epochs)
+    dict_figures = {}
+    for loss, title in zip(losses, titles):
+        filename = title.replace(" ", "_").lower()
+        fig, ax  = plt.subplots()
+        ax.plot(loss)
+        ax.set_title(f"{title}")
+        dict_figures[filename] = fig
+    dotfile = hmm_plot_graphviz(params_sgd, "hmm_casino_sgd_train")
 
-fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(20, 8))
+    return dict_figures, dotfile
 
-losses = [ losses_sgd, losses_mbgd, losses_fbgd]
-titles = [ 'Stochastic Gradient Descent', 'Mini Batch Gradient Descent', 'Full Batch Gradient Descent']
-
-for ax, (loss, title) in zip(axes.flat, zip(losses, titles)):
-    ax.plot(loss)
-    ax.set_title(f'{title}')
-plt.show()
-
-hmm_plot_graphviz(params_sgd, "hmm_casino_sgd_train")
+if __name__ == "__main__":
+    from jsl.demos.plot_utils import savefig
+    figs, dotfile = main()
+    savefig(figs)
+    plt.show()
