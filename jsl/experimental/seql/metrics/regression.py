e62a0b5930b47024542f593dfb4f9584211a11e9
karalleyna <36455180+karalleyna@users.noreply.github.com>
2022-04-08 18:13:23 +0300

    Update regression.py

--- jsl/experimental/seql/metrics/regression.py
+++ jsl/experimental/seql/metrics/regression.py
@@ -14,15 +14,18 @@ from jsl.experimental.seql.utils import gaussian_log_likelihood
 @dataclasses.dataclass
 class GaussianSampleKL(MetricFn):
     """Evaluates KL according to optimized Gaussian residual model."""
+    enn_sigma: float
 
     def __call__(self,
                  predictions: chex.Array,
                  test_data: Tuple[Tuple[chex.Array, chex.Array], float]) -> float:
         """Computes KL estimate on a single instance of test data."""
         (x, y), true_ll = test_data
+        num_enn_samples, tau, out = predictions.shape
+
         tau = x.shape[0]
         batched_err = predictions - jnp.expand_dims(y, 0)
-        chex.assert_shape(batched_err, [self.num_enn_samples, tau, 1])
+        chex.assert_shape(batched_err, [num_enn_samples, tau, 1])
 
         # ENN uses the enn_sigma to compute likelihood of sampled data
         enn_mean = jnp.zeros((tau,))
@@ -30,7 +33,7 @@ class GaussianSampleKL(MetricFn):
         batched_ll = vmap(gaussian_log_likelihood, in_axes=[None, None, 0])
 
         sampled_ll = batched_ll(enn_mean, enn_cov, batched_err)
-        chex.assert_shape(sampled_ll, [self.num_enn_samples, 1])
+        chex.assert_shape(sampled_ll, [num_enn_samples, 1])
 
         ave_ll = average_sampled_log_likelihood(sampled_ll)
         return true_ll - ave_ll
